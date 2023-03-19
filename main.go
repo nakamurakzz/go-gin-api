00243a831f648b5ed3d781a6464eb6e1df1c4e83
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-03-19 17:20:57 +0900

    API仕様書追加

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,21 +12,59 @@ import (
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	ua := ""
 
 	// UserAgentを取得するミドルウェア
 	r.Use(func(c *gin.Context) {
-		ua = c.GetHeader("User-Agent")
 		c.Next()
 	})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-			"ua":      ua,
-		})
-	})
+	api := r.Group("/api/")
+	{
+		api.GET("/sites", getSite)
+		api.POST("/sites", getSite)   // TODO: 修正
+		api.PATCH("/sites", getSite)  // TODO: 修正
+		api.DELETE("/sites", getSite) // TODO: 修正
+	}
+
 	return r
+}
+
+type site struct {
+	id       int
+	name     string
+	isEnable bool
+}
+
+func getSite(c *gin.Context) {
+	data := []site{
+		{
+			id:       1,
+			name:     "test",
+			isEnable: true,
+		},
+	}
+
+	message := "ok"
+	status := http.StatusOK
+
+	if err := errorFunc(); err != nil {
+		message = err.Error()
+		data = nil
+		status = http.StatusInternalServerError
+	}
+
+	c.IndentedJSON(status, gin.H{
+		"message": message,
+		"data":    data,
+	})
+}
+
+// 2回に1度はエラーになる関数
+func errorFunc() error {
+	if rand.Intn(2) == 0 {
+		return nil
+	}
+	return errors.New("2回に1度発生するERROR")
 }
 
 func main() {
